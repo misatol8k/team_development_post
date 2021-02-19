acb2d3ac76fc40c586bddc721ec78372d177edd1
misatol8k <misatolkurihara@gmail.com>
2021-02-19 16:57:48 +0900

    機能追加：Agendaの作者またはTeamの作者がアジェンダを削除する機能

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: %i[destroy]
 
   def index
     @agendas = Agenda.all
@@ -15,9 +15,21 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+
+  def destroy
+    if current_user.id == @agenda.user_id || current_user.id == @agenda.team.owner_id
+      @agenda.destroy
+      puts 'destroyしました'
+      # Agendaが削除されると、そのAgendaに紐づいているTeamに所属しているユーザー全員に通知メールが飛ぶ
+      # メールのインスタンス作成(Agenda.Team.所属ユーザー チーム削除メール) notice: 削除しましたメッセージ
+      redirect_to dashboard_url, notice: '削除しましたメッセージ'
+    else
+      puts 'destroyできませんでした'
     end
   end
 
